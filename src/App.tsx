3257d64bdeb38d3f84a06c147189937ac483d96c
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2024-02-15 07:42:40 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import "./sass/App.scss";
+import { useRef, useState } from "react";
+import Signin from "./Signin";
+
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const usernameInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      {loggedIn ? (
+        <Signin />
+      ) : (
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <input type="text" ref={usernameInput} placeholder="username" />
+          </div>
+          <div>
+            <input type="text" ref={passwordInput} placeholder="password" />
+          </div>
+          <button
+            type="submit"
+            onClick={() => {
+              if (
+                usernameInput.current?.value === "amir.dev" &&
+                passwordInput.current?.value === "pg67a/w"
+              )
+                setLoggedIn(true);
+            }}
+          >
+            Login
+          </button>
+        </form>
+      )}
+    </>
+  );
+};
+
+export default App;
---
+++ src/Signin.tsx
@@ -0,0 +1,7 @@
+import "./sass/Signin.scss";
+
+const Signin = () => {
+  return <h1>Welcome!</h1>;
+};
+
+export default Signin;
